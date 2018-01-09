17d12b58085eb39e287c69637c264dcd9afe5a52
djstrong <djstrong@gmail.com>
2018-01-09 12:38:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='krnnt',
       license='GPL',
       python_requires='>=3, <4',
       install_requires=[
-          'h5py','Keras','numpy','progress','progressbar2','regex','requests','setproctitle','Theano'
+          'h5py','Keras','numpy','progress','progressbar2','regex','requests','setproctitle','Theano','termcolor'
       ],
       zip_safe=False)
